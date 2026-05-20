e0b4acd40cbe9e026390293ead1d502029aaa5f0
froy33 <f90ymtz@gmail.com>
2026-05-19 21:17:09 -0600

    /

---
+++ src/app/components/mascota/mascota.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mascota',
+  imports: [],
+  templateUrl: './mascota.html',
+  styleUrl: './mascota.css',
+})
+export class Mascota {
+  listar() {
+
+  }
+
+  guardar() {
+  }
+  actualizar() {
+  }
+  eliminar() {
+  }
+
+}
